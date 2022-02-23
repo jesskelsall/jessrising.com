8dedd90e05e0d449130d99d9f182356d1b867b69
Jessica Kelsall <hi@jesskelsall.dev>
2022-02-23 21:01:22 +0000

    Boilerplate/config from dnd-ui-2

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "prettier",
+    "plugin:security/recommended",
+    "plugin:@next/next/recommended",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "security"],
+  rules: {
+    "import/extensions": "off",
+    "import/named": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "import/prefer-default-export": "off",
+    "no-console": ["warn", { allow: ["error", "info", "warn"] }],
+    "prettier/prettier": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-filename-extension": ["warn", { extensions: [".ts", ".tsx"] }],
+    "react/jsx-key": ["error", { checkFragmentShorthand: true }],
+    "react/jsx-props-no-spreading": "off",
+    "react/no-array-index-key": "warn",
+    "react/require-default-props": "off",
+  },
+};
