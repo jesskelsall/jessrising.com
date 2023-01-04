4f679292f0f428d9ec763f42eb6270a0b6dd4ce8
Jessica Kelsall <hi@jesskelsall.dev>
2023-01-04 02:03:04 +0000

    Revert gallery to getStaticProps (won't SSR on Vercel with fs)

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,5 +1,5 @@
 import { startCase } from "lodash/fp";
-import { GetServerSideProps, NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import path from "path";
@@ -13,73 +13,73 @@ import { IMarkdownData } from "../../types";
 
 interface IProps {
   galleryPhotos: IMarkdownData[];
-  locations: string[];
-  tags: string[];
+  // locations: string[];
+  // tags: string[];
 }
 
-export const getServerSideProps: GetServerSideProps<IProps> = async ({
+export const getStaticProps: GetStaticProps<IProps> = async (/* {
   query,
-}) => {
+} */) => {
   const galleryPhotos = await getAllGalleryPhotos();
 
-  // Deconstruct query params
-  const tags = formatFilterQuery(query.t);
-  const locations = formatFilterQuery(query.l);
+  // // Deconstruct query params
+  // const tags = formatFilterQuery(query.t);
+  // const locations = formatFilterQuery(query.l);
 
   // Apply filters and sorting
-  const displayGalleryPhotos = applyFilterQueries<IMarkdownData>(
-    galleryPhotos,
-    [tags, (photo) => photo.meta.tags || []],
-    [locations, (photo) => photo.meta.locations || []]
-  ).sort(sortGalleryPhotosByDate);
+  // const displayGalleryPhotos = applyFilterQueries<IMarkdownData>(
+  //   galleryPhotos,
+  //   [tags, (photo) => photo.meta.tags || []],
+  //   [locations, (photo) => photo.meta.locations || []]
+  // ).sort(sortGalleryPhotosByDate);
+  const displayGalleryPhotos = galleryPhotos.sort(sortGalleryPhotosByDate);
 
   return {
     props: {
       galleryPhotos: displayGalleryPhotos,
-      locations,
-      tags,
+      // locations,
+      // tags,
     },
   };
 };
 
-const GalleryPage: NextPage<IProps> = ({ galleryPhotos, locations, tags }) => {
-  const filteredByLocation = Boolean(locations.length);
-  const filteredByTag = Boolean(tags.length);
+const GalleryPage: NextPage<IProps> = ({
+  galleryPhotos /* locations, tags */,
+}) => (
+  // const filteredByLocation = Boolean(locations.length);
+  // const filteredByTag = Boolean(tags.length);
 
-  return (
-    <>
-      <h1>Gallery</h1>
+  <>
+    <h1>Gallery</h1>
 
-      {/* Filters */}
-      {(filteredByLocation || filteredByTag) && (
+    {/* Filters */}
+    {/* {(filteredByLocation || filteredByTag) && (
         <ul>
           {filteredByLocation && <li>Locations: {locations.join(", ")}</li>}
           {filteredByTag && <li>Tags: {tags.join(", ")}</li>}
         </ul>
-      )}
+      )} */}
 
-      {/* Gallery grid */}
-      <div style={{ display: "flex", flexWrap: "wrap", gap: 20 }}>
-        {galleryPhotos.map(({ slug }) => {
-          const imagePath = path.join("/photos", `${slug}.jpeg`);
+    {/* Gallery grid */}
+    <div style={{ display: "flex", flexWrap: "wrap", gap: 20 }}>
+      {galleryPhotos.map(({ slug }) => {
+        const imagePath = path.join("/photos", `${slug}.jpeg`);
 
-          return (
-            <Link href={`/gallery/${slug}`} key={slug}>
-              <div
-                style={{
-                  height: 375,
-                  position: "relative",
-                  width: 500,
-                }}
-              >
-                <Image alt={startCase(slug)} src={imagePath} fill />
-              </div>
-            </Link>
-          );
-        })}
-      </div>
-    </>
-  );
-};
-
+        return (
+          <Link href={`/gallery/${slug}`} key={slug}>
+            <div
+              style={{
+                height: 375,
+                position: "relative",
+                width: 500,
+              }}
+            >
+              <Image alt={startCase(slug)} src={imagePath} fill />
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  </>
+);
 export default GalleryPage;
