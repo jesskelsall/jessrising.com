edd3ebab19b09ab29acd5356cad3ed4a65e52368
Jessica Kelsall <hi@jesskelsall.dev>
2023-01-16 21:20:38 +0000

    Blog list preview images

--- components/BlogPreview/BlogPreview.tsx
+++ components/BlogPreview/BlogPreview.tsx
@@ -11,19 +11,29 @@ export const BlogPreview = ({ blogPost }: IBlogPreviewProps) => {
   const { date, slug, summary } = blogPost;
 
   return (
-    <>
-      <h2>
-        <Link href={`/blog/p/${slug}`}>{summary.heading}</Link>
-      </h2>
-      <p>
-        {date && (
-          <>
-            <em>{formatFullDate(DateTime.fromISO(date))}</em>
-            <br />
-          </>
-        )}
-        {summary.paragraph}
-      </p>
-    </>
+    <div style={{ display: "flex", marginBottom: 30 }}>
+      <div>
+        <h2 style={{ marginTop: 0 }}>
+          <Link href={`/blog/p/${slug}`}>{summary.heading}</Link>
+        </h2>
+        <p>
+          {date && (
+            <>
+              <em>{formatFullDate(DateTime.fromISO(date))}</em>
+              <br />
+            </>
+          )}
+          {summary.paragraph}
+        </p>
+      </div>
+      {summary.imageSlug && (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={`photos/${summary.imageSlug}.jpeg`}
+          alt={summary.heading}
+          style={{ maxHeight: 150, maxWidth: 200 }}
+        />
+      )}
+    </div>
   );
 };
