a5a2149118aab97067ed172298e2afbf9df076bf
Jessica Kelsall <hi@jesskelsall.dev>
2023-02-22 22:48:58 +0000

    Scroll restoration for gallery back

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  experimental: { scrollRestoration: true },
   reactStrictMode: true,
   staticPageGenerationTimeout: 60,
   async redirects() {
