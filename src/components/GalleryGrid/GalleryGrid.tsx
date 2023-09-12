62434b37b9bab44d79435187d091004cc1e08a23
Jessica Kelsall <hi@jesskelsall.dev>
2023-09-13 00:08:50 +0100

    Portrait gallery photo support

--- src/components/GalleryGrid/GalleryGrid.tsx
+++ src/components/GalleryGrid/GalleryGrid.tsx
@@ -10,15 +10,23 @@ interface IGalleryGridProps {
 
 export const GalleryGrid = ({ galleryPhotos }: IGalleryGridProps) => (
   <div className="gallery-grid">
-    {galleryPhotos.map(({ slug, title }) => (
-      <Link href={`/gallery/p/${slug}`} key={slug}>
-        <Image
-          alt={title}
-          src={getImageSrcFromSlug(slug, PHOTO_SIZE_SUFFIX.SMALL)}
-          height="375"
-          width="500"
-        />
-      </Link>
-    ))}
+    {galleryPhotos.map(({ exif, slug, title }) => {
+      const { dimensions } = exif;
+      const isLandscape = dimensions
+        ? dimensions.width >= dimensions.height
+        : true;
+
+      return (
+        <Link href={`/gallery/p/${slug}`} key={slug}>
+          <Image
+            alt={title}
+            className={isLandscape ? "landscape" : "portrait"}
+            src={getImageSrcFromSlug(slug, PHOTO_SIZE_SUFFIX.SMALL)}
+            height="375"
+            width="500"
+          />
+        </Link>
+      );
+    })}
   </div>
 );
