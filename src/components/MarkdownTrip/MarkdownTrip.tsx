7ef081d00f98b6b54b30bcc906f2079c6510479a
Jessica Kelsall <hi@jesskelsall.dev>
2023-10-29 20:48:50 +0000

    Trip emoji on gallery photos

--- src/components/MarkdownTrip/MarkdownTrip.tsx
+++ src/components/MarkdownTrip/MarkdownTrip.tsx
@@ -3,6 +3,7 @@ import { Trip } from "../../types/trip";
 
 export const MarkdownTrip = ({ trip }: { trip: Trip }) => (
   <li>
-    Trip: <Link href={`/gallery?trip=${trip.slug}`}>{trip.title}</Link>
+    Trip: {trip.icon && <span className="emoji">{trip.icon}&nbsp;</span>}
+    <Link href={`/gallery?trip=${trip.slug}`}>{trip.title}</Link>
   </li>
 );
