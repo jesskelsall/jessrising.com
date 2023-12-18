9f58561e7e7afd825d1a279a7fcc175ac0b9db04
Jessica Kelsall <hi@jesskelsall.dev>
2023-12-18 00:22:37 +0000

    Fix homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const blogPostsData = blogPostsJSON as TMarkdownDataFile;
 
 // If empty strings, the most recent is used instead
 const FEATURED_BLOG_POST = "2022-12-31-favourite-photos-2022";
-const FEATURED_PHOTO = GalleryPhotoSlug.parse("stellar-watercolour");
+const FEATURED_PHOTO = GalleryPhotoSlug.parse("kenmore-waterfront-231202");
 
 interface IProps {
   blogPost: IMarkdownData;
